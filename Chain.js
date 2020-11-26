72e850209f5a03aeec45443b3417b9600f3a49e1
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2020-11-26 16:11:01 +0530

    Update Chain.js

--- Chain.js
+++ Chain.js
@@ -1,20 +1,25 @@
 class Chain{
-    constructor(bodyA, bodyB){
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
-        this.chain = Constraint.create(options);
-        World.add(world, this.chain);
+        this.pointB = pointB
+        this.Chain= Constraint.create(options);
+        World.add(world, this.Chain);
     }
 
-    display(){
-        var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
-        strokeWeight(4);
-        line(pointA.x, pointA.y, pointB.x, pointB.y);
+      display(){
+            var pointA = this.Chain.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        
     }
     
 }
+
+
+
