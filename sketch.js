f8568cc1f6f70ff9e34c9f653058911ca26dbfda
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2020-11-26 16:10:21 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -38,8 +38,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    //log6 = new Log(230,180,80, PI/2);
+    Chain = new Chain(bird.body,{x:200, y:100});
 }
 
 function draw(){
@@ -63,6 +63,6 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    //log6.display();
+    Chain.display();    
 }
