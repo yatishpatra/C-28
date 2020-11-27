e6bd3841473d72e0a59584f227fc44b2e64b9478
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2020-11-27 13:23:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-var bird, slingShot;
+var bird, SlingShot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -39,7 +39,7 @@ function setup(){
     bird = new Bird(100,100);
 
     //log6 = new Log(230,180,80, PI/2);
-    Chain = new Chain(bird.body,{x:200, y:100});
+    SlingShot = new SlingShot(bird.body,{x:200, y:100});
 }
 
 function draw(){
@@ -64,5 +64,5 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    Chain.display();    
+    SlingShot.display();    
 }
